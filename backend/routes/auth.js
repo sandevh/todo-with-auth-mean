0f98d0e799eb724f745dd46dda165de4456eea49
sandevh <sandevhettiarachchi@gmail.com>
2025-05-14 14:51:17 +0530

    created routes for authentication

---
+++ backend/routes/auth.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { Router } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+
+const JWT_SECRET = '123meantodo321';
+
+const router = Router();
+
+router.post('signup', async (req, res) => {
+  const {email, password} = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  await User.create({email, password: hashed});
+  res.send({message: "User regisdtered successfully"});
+});
+
+router.post('login', async (req, res) => {
+  const {email, password} = req.body;
+  const user  = await User.findOne({email});
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).send({message: "Invalid credentials"});
+  }
+
+  const token = jwt.sign({id: user._id}, JWT_SECRET);
+  res.send({token});
+});  
+
+export default router;
