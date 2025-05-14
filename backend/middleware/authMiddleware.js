6229a202d8efe1842332f1557f82d32d21926936
sandevh <sandevhettiarachchi@gmail.com>
2025-05-14 15:21:49 +0530

    debugged and fixed major issues found in backend

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export const authMiddleware = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split('')[1];
   if (!token) {
     return res.sendStatus(401).json({ message: 'Unauthorized' });
@@ -16,3 +16,5 @@ export const authMiddleware = (req, res, next) => {
     res.status(401).json({ message: 'Invalid token' });
   }
 };
+
+export default authMiddleware;
