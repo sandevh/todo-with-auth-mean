e044ec4915ecde0f7c1342f32e283d580c62f9a4
sandevh <sandevhettiarachchi@gmail.com>
2025-05-14 18:32:16 +0530

    created todo service

--- frontend/src/app/services/todo.service.ts
+++ frontend/src/app/services/todo.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private baseUrl = 'http://localhost:5000/api/todo';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getHeaders() {
+    const token = localStorage.getItem('token');
+    return {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${token}`),
+    };
+  }
+
+  getTodos() {
+    return this.http.get(this.baseUrl, this.getHeaders());
+  }
+
+  addTodo(task: string) {
+    return this.http.post(this.baseUrl, {task}, this.getHeaders());
+  }
+
+  deleteTodo(id: string) {
+    return this.http.delete(`${this.baseUrl}/${id}`, this.getHeaders())
+  }
 }
