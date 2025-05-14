51b60d640e5224145dc75de5c5acd55b4ee0460b
sandevh <sandevhettiarachchi@gmail.com>
2025-05-14 18:32:33 +0530

    implemented and registered routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  {path: '', redirectTo: 'todo', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'todo', component: TodoComponent, canActivate: [AuthGuard]},
+];
