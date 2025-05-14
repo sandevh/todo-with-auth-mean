0c94431b49e5516a4eba8e7bbb1b3ac83f09eb7c
sandevh <sandevhettiarachchi@gmail.com>
2025-05-14 14:54:42 +0530

    implemented auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = "123meantodo321";
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split('')[1];
+  if (!token) {
+    return res.sendStatus(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
